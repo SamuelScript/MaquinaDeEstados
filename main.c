6ebd7ded8d39f6ae89110a353aa55043caf86c42
Samuel <samuelbernabefarias@gmail.com>
2022-09-01 21:37:08 -0300

    Foram criadas uma struct de elemento de contexto onde as terá indices que apontarão para os próximos estados, o elemento de entrada do alfabeto de entrada, o elemento de saida do alfabeto de saida, refatoração do método link_states.

--- main.c
+++ main.c
@@ -1,68 +1,95 @@
 #include <stdio.h>
 #include <malloc.h>
-
-struct state_func;
-struct state;
-
-char alf_in[2][1] = {"0", "1"};
-char alf_out[2][1] = {"O","I"};
-
-typedef struct {
+struct state_func{
     char in;
     char out;
     struct state *next;
-} state_func;
-typedef struct {
-    state_func funcs[2];
-} state;
+};
+struct state{
+    char *name;
+    struct state_func *funcs;
+};
+struct elemento_contexto {
+    int *in;
+    int *out;
+    int *state;
+};
 
-state states[4];
+int n_states = 4;
+int n_func = 3;
 
-void def_func(state_func *stt_f ,char in, char out, state *next) {
+struct state *states;
+struct elemento_contexto *ec;
+
+char alf_in[3][1] = {"0", "1","x"};
+char alf_out[3][1] = {"A","B", "X"};
+
+void def_func(struct state_func *stt_f ,char in, char out, struct state *next) {
     stt_f->in = in;
     stt_f->out = out;
     stt_f->next = next;
 }
-void link_states() {
-    state_func func0[4], func1[4];
-    def_func((&func0[0]),alf_in[0][0],alf_out[0][0],&(states[1]));
-    def_func((&func0[1]),alf_in[0][0],alf_out[1][0],&(states[2]));
-    def_func((&func0[2]),alf_in[0][0],alf_out[1][0],&(states[3]));
-    def_func((&func0[3]),alf_in[0][0],alf_out[0][0],&(states[0]));
-    states[0].funcs[0] = func0[0];
-    states[1].funcs[0] = func0[1];
-    states[2].funcs[0] = func0[2];
-    states[3].funcs[0] = func0[3];
-    def_func((&func1[0]),alf_in[1][0],alf_out[1][0],&(states[2]));
-    def_func((&func1[1]),alf_in[1][0],alf_out[0][0],&(states[3]));
-    def_func((&func1[2]),alf_in[1][0],alf_out[1][0],&(states[0]));
-    def_func((&func1[3]),alf_in[1][0],alf_out[0][0],&(states[1]));
-    states[0].funcs[1] = func1[0];
-    states[1].funcs[1] = func1[1];
-    states[2].funcs[1] = func1[2];
-    states[3].funcs[1] = func1[3];
+void link_states(int n,int n_f,struct elemento_contexto *m) {
+    struct state_func func[n_f][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n_f; j++) {
+            def_func((&func[j][i]),alf_in[m[i].in[j]][0],alf_out[m[i].out[j]][0],&(states[m[i].state[j]]));
+            states[i].funcs[j] = func[j][i];
+        }
+    }
 }
 
 int main() {
-    link_states();
-    state *stt = &states[0];
-    char txt[2] = "11";
-    for(int i = 0 ; i < 2; i++) {
+    states = malloc(n_states*sizeof (struct state));
+    ec = malloc(n_states*sizeof (struct elemento_contexto));
+    int out[] = {0,1,2,1,0,2,1,1,2,0,0,2};
+    char *names[] = {"q0","q1","q2","q3"};
+
+    for(int i = 0; i < n_states; i++) {
+        states[i].name = names[i];
+        states[i].funcs = malloc(n_func*sizeof (struct state_func));
+
+        ec[i].in = malloc(n_func*sizeof (int));
+        ec[i].out = malloc(n_func*sizeof (int));
+        ec[i].state = malloc(n_func*sizeof (int));
+
+        for(int j = 0; j < n_func; j++) {
+            ec[i].in[j] = j;
+            ec[i].out[j] = out[i*3 + j];
+            ec[i].state[j] = i + j + 1 - ((i + j >= n_func)?n_func:0);
+        }
+
+    }
+
+    link_states(n_states,n_func,ec);
+    struct state *stt = &states[0];
+    char txt[5] = "11x00";
+
+    printf("Lendo fita.... \n");
+    for(int i = 0 ; i < 5; i++) {
+        printf("%s , %c\n",stt->name,txt[i]);
         if(txt[i] == stt->funcs[0].in) {
             txt[i] = stt->funcs[0].out;
             stt = stt->funcs[0].next;
         } else if(txt[i] == stt->funcs[1].in) {
             txt[i] = stt->funcs[1].out;
             stt = stt->funcs[1].next;
+        } else if(txt[i] == stt->funcs[2].in) {
+            txt[i] = stt->funcs[2].out;
+            stt = stt->funcs[2].next;
         } else {
             printf("Entrada inesperada!\n");
         }
     }
-    printf("Lendo fita.... \n");
     printf("%s \n",txt);
     printf("Fim da execução.... \n");
-    for(int i = 0; i < 4; i++){
-        free((&states[i]));
+    for(int i = 0; i < n_states; i++) {
+        free(states[i].funcs);
+        free(ec[i].in);
+        free(ec[i].out);
+        free(ec[i].state);
     }
+    free(states);
+    free(ec);
     return 0;
 }
