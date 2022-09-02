e5d4d209a09fc6fdc2fc4a2811b5c0c866da2094
Samuel <samuelbernabefarias@gmail.com>
2022-09-02 10:51:56 -0300

    Add struct maquina, contend os alfabetos que a maquina de estados irá usar.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+
 struct state_func{
     char in;
     char out;
@@ -14,37 +15,51 @@ struct elemento_contexto {
     int *out;
     int *state;
 };
+struct string{
+    char *self;
+};
+struct maquina {
+    struct string *alfa_in;
+    struct string *alfa_out;
+};
 
 int n_states = 4;
 int n_func = 3;
+int n_alfa_in = 3;
+int n_alfa_out = 3;
 
 struct state *states;
 struct elemento_contexto *ec;
-
-char alf_in[3][1] = {"0", "1","x"};
-char alf_out[3][1] = {"A","B", "X"};
+struct maquina m;
 
 void def_func(struct state_func *stt_f ,char in, char out, struct state *next) {
     stt_f->in = in;
     stt_f->out = out;
     stt_f->next = next;
 }
-void link_states(int n,int n_f,struct elemento_contexto *m) {
+void link_states(int n,int n_f,struct elemento_contexto *e_c) {
     struct state_func func[n_f][n];
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n_f; j++) {
-            def_func((&func[j][i]),alf_in[m[i].in[j]][0],alf_out[m[i].out[j]][0],&(states[m[i].state[j]]));
+            def_func((&func[j][i]),m.alfa_in[e_c[i].in[j]].self[0],m.alfa_out[e_c[i].out[j]].self[0],&(states[e_c[i].state[j]]));
             states[i].funcs[j] = func[j][i];
         }
     }
 }
 
 int main() {
+    m.alfa_in = malloc(n_alfa_in*sizeof (struct string));
+    m.alfa_out = malloc(n_alfa_out*sizeof (struct string));
     states = malloc(n_states*sizeof (struct state));
     ec = malloc(n_states*sizeof (struct elemento_contexto));
+
     int out[] = {0,1,2,1,0,2,1,1,2,0,0,2};
     char *names[] = {"q0","q1","q2","q3"};
+    char *a_in[] = {"0", "1","x"};
+    char *a_out[] = {"A","B", "X"};
 
+    for(int i = 0; i < n_alfa_in; i++) m.alfa_in[i].self = a_in[i];
+    for(int i = 0; i < n_alfa_out; i++) m.alfa_out[i].self = a_out[i];
     for(int i = 0; i < n_states; i++) {
         states[i].name = names[i];
         states[i].funcs = malloc(n_func*sizeof (struct state_func));
@@ -55,7 +70,7 @@ int main() {
 
         for(int j = 0; j < n_func; j++) {
             ec[i].in[j] = j;
-            ec[i].out[j] = out[i*3 + j];
+            ec[i].out[j] = out[i*n_func + j];
             ec[i].state[j] = i + j + 1 - ((i + j >= n_func)?n_func:0);
         }
 
@@ -89,6 +104,8 @@ int main() {
         free(ec[i].out);
         free(ec[i].state);
     }
+    free(m.alfa_in);
+    free(m.alfa_out);
     free(states);
     free(ec);
     return 0;
